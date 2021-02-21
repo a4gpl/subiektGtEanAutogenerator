202f8c69e4d2978731701fdec2fc0a84342e52a3
Michał Sagan <michal.sagan@icloud.com>
2021-02-21 11:14:20 +0100

    Create test for function fnCyfraKontrolnaEan.sql

---
+++ test/fnCyfraKontrolnaEan.sql
@@ -0,0 +1,3 @@
+SELECT dbo.fnCyfraKontrolnaEan('500029962803'); -- Ean 5000299628034 cyfra kontrolna (4)
+SELECT dbo.fnCyfraKontrolnaEan('544900013332'); -- Ean 5449000133328 cyfra kontrolna (8)
+SELECT dbo.fnCyfraKontrolnaEan('590063500002'); -- Ean 5900635000022 cyfra kontrolna (2)
